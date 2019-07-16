6e2d7caa80bc7c21f844403fee94393dbae3cbe4
Pat Haynes <pathaynes32@gmail.com>
2019-07-16 11:55:12 -0700

    couple more things add

--- test/tests.js
+++ test/tests.js
@@ -1,12 +1,16 @@
-function isYes(words) {
-    words = words.toLowerCase();
-    if(words === 'yes') {
-        return true;
-    }
+import isYes from '../src/is-yes.js';
+const test = QUnit.test;
 
-    else if(words === 'no') {
-        return false;
-    }
-}
+test('returns true if yes', function(assert) {
+    //Arrange
+    const answer = 'yes';
+    const expected = true;
+    // Set up your parameters and expectations
 
-export default isYes;
+    //Act 
+    const result = isYes(answer);
+    // Call the function you're testing and set the result to a const
+
+    //Assert
+    assert.equal(result, expected);
+});
