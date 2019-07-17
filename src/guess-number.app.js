60ada7a823eb8cbf7baf4585c8e0ff55b44a4284
Pat Haynes <pathaynes32@gmail.com>
2019-07-17 11:51:58 -0700

    cleaned up code

--- src/guess-number.app.js
+++ src/guess-number.app.js
@@ -14,42 +14,47 @@ const highLow = document.getElementById('high-low');
 const correctNumber = 16;
 const youFailed = document.getElementById('failure');
 
-//
-let triesCount = remainingTries.textContent;
 
+//variable for countdown
+
+
+
+let triesCount = 3;
 
 // beginning of function
-
 clickHere.addEventListener('click', () => {
-    let guess = guessNumber.value; 
-    triesCount -= 1;
+    
+    let guess = parseInt(guessNumber.value);
     remainingTries.textContent = triesCount;
-    if(triesCount == 0){
-        clickHere.disabled = true;
-    }
-
-    //Failure Statement
-    if(triesCount == 0){
-        youFailed.textContent = 'Go home you boolean';
-    }
-
-
+    
+    
+    
+    
     // Correct Answer Output
-    if(compareNumbers(guess, correctNumber) == 0){
+    if(compareNumbers(guess, correctNumber) === 0){
         highLow.textContent = 'You Nailed it!';
         resultNumber.textContent = 'We have a winner!!!!';
         clickHere.disabled = true;
-
-    } else if(compareNumbers(guess, correctNumber) == -1){
+        
+    } else if(compareNumbers(guess, correctNumber) === -1){
         resultNumber.textContent = 'Nice try dude, but that was wrong!';
         highLow.textContent = 'You guessed to low!';
-    
-    }  
-
-    else if(compareNumbers(guess, correctNumber) == +1){
+        triesCount -= 1;
+        
+    } else if(compareNumbers(guess, correctNumber) === +1){
         resultNumber.textContent = 'Maybe you should leave';
         highLow.textContent = 'Woah, way to high man!';
+        triesCount -= 1;
+
     }
+    
+    if(triesCount === -1){
+        clickHere.disabled = true;
+        highLow.hidden = true;
+        resultNumber.hidden = true;
+        youFailed.textContent = 'Go home you boolean!';
+    }
+
 
 });
 
