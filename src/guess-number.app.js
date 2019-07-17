ed0e9e6ad27e9bb373b4e1963d55a23400b2f50b
Pat Haynes <pathaynes32@gmail.com>
2019-07-16 22:36:37 -0700

    css edits

---
+++ src/guess-number.app.js
@@ -0,0 +1,9 @@
+// What do I need to import from other js files?
+import compareNumbers from './guess-number.js';
+
+// What do I need from the DOM (elements)
+const clickHere = document.getElementById('click');
+const resultNumber = document.getElementById('result');
+const remainingTries = document.getElementById('remaining-tries');
+const highLow = document.getElementById('high-low');
+
