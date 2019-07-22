a3d89bfc18c7e7ed8861b522594622da25d28172
Pat Haynes <pathaynes32@gmail.com>
2019-07-22 09:12:16 -0700

    cleaned up code on guess-number

--- src/guess-number.app.js
+++ src/guess-number.app.js
@@ -1,11 +1,5 @@
-// What do I need to import from other js files?
 import compareNumbers from './guess-number.js';
 
-
-
-
-
-// What do I need from the DOM (elements)
 const clickHere = document.getElementById('click');
 const guessNumber = document.getElementById('guess-number');
 const resultNumber = document.getElementById('result');
@@ -14,23 +8,13 @@ const highLow = document.getElementById('high-low');
 const correctNumber = 16;
 const youFailed = document.getElementById('failure');
 
-
-//variable for countdown
-
-
-
 let triesCount = 3;
 
-// beginning of function
 clickHere.addEventListener('click', () => {
     
     let guess = parseInt(guessNumber.value);
     remainingTries.textContent = triesCount;
     
-    
-    
-    
-    // Correct Answer Output
     if(compareNumbers(guess, correctNumber) === 0){
         highLow.textContent = 'You Nailed it!';
         resultNumber.textContent = 'We have a winner!!!!';
@@ -45,7 +29,6 @@ clickHere.addEventListener('click', () => {
         resultNumber.textContent = 'Maybe you should leave';
         highLow.textContent = 'Woah, way to high man!';
         triesCount -= 1;
-
     }
     
     if(triesCount === -1){
@@ -54,7 +37,5 @@ clickHere.addEventListener('click', () => {
         resultNumber.hidden = true;
         youFailed.textContent = 'Go home you boolean!';
     }
-
-
 });
 
--- src/guess-number.js
+++ src/guess-number.js
@@ -1,5 +1,4 @@
 
-// What do I need to import from other js files?
 function compareNumbers(guess, correctNumber){
     if(guess < correctNumber) {
         return -1;
@@ -10,12 +9,6 @@ function compareNumbers(guess, correctNumber){
     } else if(guess === correctNumber) {
         return 0;
     }
- 
 }
-// What do I need from the DOM (elements)
-
-//What state do I need to track?
-
-//What events do I need to listen to?
 
 export default compareNumbers;
