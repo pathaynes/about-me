385203e989e3bcebf5cf1356b296e38b2079c8b3
Pat Haynes <pathaynes32@gmail.com>
2019-07-22 09:23:02 -0700

    made suggested changes for re-submitting

--- test/guess-number.test.js
+++ test/guess-number.test.js
@@ -2,7 +2,7 @@ import compareNumbers from '../src/guess-number.js';
 
 const test = QUnit.test;
 
-test('compare-number', (assert) => {
+test('compare-number-10', (assert) => {
     const guess = 10;
     const correctNumber = 32;
     const expected = -1;
@@ -11,20 +11,29 @@ test('compare-number', (assert) => {
     assert.equal(result, expected);
 });
 
-test('compare-number', (assert) => {
-    const guess = 10;
-    const correctNumber = 32;
-    const expected = -1;
+test('compare-number-16', (assert) => {
+    const guess = 16;
+    const correctNumber = 10;
+    const expected = +1;
     const result = compareNumbers(guess, correctNumber);
 
     assert.equal(result, expected);
 });
 
 
-test('compare-number', (assert) => {
+test('compare-number-32', (assert) => {
     const guess = 32;
     const correctNumber = 32;
     const expected = 0;
+    const result = compareNumbers(guess, correctNumber);
+
+    assert.equal(result, expected);
+});
+
+test('compare-number-55', (assert) => {
+    const guess = 55;
+    const correctNumber = 25;
+    const expected = +1;
     const result = compareNumbers(guess, correctNumber);
 
     assert.equal(result, expected);
